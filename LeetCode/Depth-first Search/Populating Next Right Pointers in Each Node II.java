06249b633daa027bc2e926225b6c5a5774c2768a
ppyppy <pingpingya@gmail.com>
2014-11-20 00:07:04 -0800

    Update Populating Next Right Pointers in Each Node II.java

--- LeetCode/Depth-first Search/Populating Next Right Pointers in Each Node II.java
+++ LeetCode/Depth-first Search/Populating Next Right Pointers in Each Node II.java
@@ -27,6 +27,29 @@
  *     TreeLinkNode(int x) { val = x; }
  * }
  */
+ 
+     public void connect(TreeLinkNode root) {
+        TreeLinkNode cur=root, head=null, prev=null;
+        while(cur!=null) {
+            while(cur!=null) {
+                if(cur.left!=null) {
+                    if(prev!=null) prev.next = cur.left;
+                    else head = cur.left;
+                    prev=cur.left;
+                }
+                if(cur.right!=null) {
+                    if(prev!=null) prev.next = cur.right;
+                    else head = cur.right;
+                    prev=cur.right;
+                }
+                cur=cur.next;
+            }
+            cur=head;
+            head=null;
+            prev=null;
+        }
+    }
+    
 public class Solution {
     public void connect(TreeLinkNode root) {
         if(root==null)
