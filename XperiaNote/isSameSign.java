e6c1d17bfb44ed6181cdfe84675cdd0e1d5518cd
ppyppy <pingpingya@gmail.com>
2014-12-09 22:34:46 -0800

    Create isSameSign.java

---
+++ XperiaNote/isSameSign.java
@@ -0,0 +1,9 @@
+Question:
+Given two integers, write a function that checks to see if the two numbers are the same sign using
+bitwise operators.
+Answer:
+1. public class Solution43 {
+2. public static boolean isSameSign(int x, int y) {
+3.      return ((x ^ y) > 0);
+4. }
+5. }
