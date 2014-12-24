5534ed8152feebbd2c396d8133cb6d99705109f9
Eric Yang <pingpingya@gmail.com>
2014-12-24 11:40:07 -0800

    Update 4_1_IsBalancedTree.java

--- CareerCup/4_1_IsBalancedTree.java
+++ CareerCup/4_1_IsBalancedTree.java
@@ -1,15 +1,43 @@
-boolean isBalancedTree(TreeNode T){
-		return ( maxDepth(T)-minDepth(T) )<=1;
+int getHeight(TreeNode root){
+	if(root==null)
+		return 0;
+	return 1 + Math.max( getHeight(root.left), getHeight(root.right) );
 }
 
-int maxDepth(TreeNode T){
+boolean isBalanced(TreeNode T){
 	if(T==null)
-		return 0;
-	return Math.max(maxDepth(T.left), maxDepth(T.right))+1;
+		return true;
+	int diff = Math.abs( getHeight(T.left) - getHeight(T.right) );
+	if(diff>1)
+		return false;
+	return isBalanced(T.left)&&isBalanced(T.right);
 }
 
-int minDepth(TreeNode T){
+Although this works, it's not very efficient. On each node, we recurse through its entire subtree.
+This means that getHeight is called repeatedly on the same nodes.The algorithm is therefore O(N^2).
+
+boolean isBalanced(TreeNode T){
+	if( checkHeight(T)==-1 )
+		return false;
+	else
+		return true;
+}
+
+int checkHeight(TreeNode T){
 	if(T==null)
 		return 0;
-	return Math.min(minDepth(T.left), minDepth(T.right))+1;
+	int lh = checkHeight(T.left);
+	if(lh==-1)
+		return -1;
+	int rh = checkHeight(T.right);
+	if(rh==-1)
+		return -1;
+	if(Math.abs(lh-rh)>1)
+		return -1;
+	return Math.max(lh, rh)+1;
 }
+
+Go through every node only once for the height, once child node is not balanced (-1), all the parent node will return -1.
+So it is O(n), space is O(h).
+
+
