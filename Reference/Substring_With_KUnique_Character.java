b7313ccf1658aa156db4e8ddc360cd900aa564e0
Eric Yang <pingpingya@gmail.com>
2015-11-19 22:14:56 -0800

    Update Substring_With_KUnique_Character.java

--- Reference/Substring_With_KUnique_Character.java
+++ Reference/Substring_With_KUnique_Character.java
@@ -1,34 +1,48 @@
 	Given a string, find the longest substring that contains only two unique characters. For example, given "abcbbbbcccbdddadacb", 
 	the longest substring that contains 2 unique character is "bcbbbbcccb".
 	
-	public static String maxSubString2UniqueChars(String s) {
+	public static String maxSubStringKUniqueChars(String s, int k) {
+		//declare a counter
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();		
+		int start = 0;
 		int maxLen = 0;
 		String maxSubstring = null;
-		int start = 0;
-		// declare a map to track the right most position of the two characters
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+	 
 		for (int i = 0; i < s.length(); i++) {
+			//add each char to the counter
 			char c = s.charAt(i);
-			// if map contains 2 characters, process
-			if (map.size() == 2 && !map.containsKey(c)) {
-				if (i - start > maxLen) {
-					maxLen = i - start;
+			if(map.containsKey(c)){
+				map.put(c, map.get(c)+1);
+			}else{
+				map.put(c, 1);
+			}
+	 
+			if(map.size() == k+1){
+				//get maximum
+				int range = i-start;
+				if(range > maxLen){
+					maxLen = range;
 					maxSubstring = s.substring(start, i);
 				}
-				// get the left most index for
-				int leftMost = s.length();
-				for (Entry<Character, Integer> entry : map.entrySet()) {
-					if (entry.getValue() < leftMost) {
-						leftMost = entry.getValue();
+	 
+				//move left cursor toward right, so that substring contains only k chars
+				while(map.size()>k){
+					char first = s.charAt(start);
+					int count = map.get(first);
+					if(count>1){
+						map.put(first,count-1);
+					}else{
+						map.remove(first);
 					}
+					start++;
 				}
-				start = leftMost + 1;
-				map.remove(s.charAt(leftMost));
 			}
-			map.put(c, i);
 		}
-		if (map.size() == 2 && maxLen == 0) {
-			return s;
+	 
+		if (map.size() == k) {
+			if(s.length()-start>maxLen)
+				maxSubstring = s.substring(start, s.length());
 		}
+	 
 		return maxSubstring;
 	}
