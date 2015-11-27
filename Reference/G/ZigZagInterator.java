89ab5259990cab4ff56af5b9fd62995068697789
Eric Yang <pingpingya@gmail.com>
2015-11-26 22:25:49 -0800

    Update ZigZagInterator.java

--- Reference/G/ZigZagInterator.java
+++ Reference/G/ZigZagInterator.java
@@ -32,3 +32,40 @@ class ZigZagIterator{
 	    	return !iterators.isEmpty();
 	    }
 }
+
+
+//using ListIterator<Iterator<Integer>> iterator
+
+	    List<Iterator<Integer>> collection = new ArrayList<Iterator<Integer>>();
+	    ListIterator<Iterator<Integer>> iterator;
+	    int index = 0;
+	    public void ZigzagIterator(List<Integer> v1, List<Integer> v2, List<Integer> v3, List<Integer> v4) {  
+	    	if(v1.iterator().hasNext())
+	    		collection.add(v1.iterator());
+	    	if(v2.iterator().hasNext())
+	    		collection.add(v2.iterator());
+	    	if(v3.iterator().hasNext())
+	    		collection.add(v3.iterator());
+	    	if(v4.iterator().hasNext())
+	    		collection.add(v4.iterator());
+	    	iterator = collection.listIterator();
+	    }
+	    
+	    public void backToBegin(){
+	    	while(iterator.hasPrevious())
+	    		iterator.previous();
+	    }
+	    
+	    public int next() {
+	    	Iterator<Integer> curIt = iterator.next();
+	    	int ret = curIt.next();
+	    	if(!curIt.hasNext())
+	    		iterator.remove();
+	    	if(!iterator.hasNext())
+	    		backToBegin();
+	    	return ret;
+	    }
+	    
+	    public boolean hasNext() {
+	    	return iterator.hasNext();
+	    }
