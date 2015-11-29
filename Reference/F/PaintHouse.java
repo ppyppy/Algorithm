9a84046871696de7a6d43c5339bcef5b86b09bea
Eric Yang <pingpingya@gmail.com>
2015-11-29 00:03:32 -0800

    Update PaintHouse.java

--- Reference/F/PaintHouse.java
+++ Reference/F/PaintHouse.java
@@ -13,7 +13,7 @@
 			   cost[i][1] = cost[i][1] + Math.min(cost[i-1][0], cost[i-1][2]);
 			   cost[i][2] = cost[i][2] + Math.min(cost[i-1][0], cost[i-1][1]);
 		   }
-		   return Math.min(cost[cost.length-1][0], Math.min(cost[cost.length-1][1], cost[cost.length-1][2]));
+		   return Math.min(cost[cost.length-1][0], Math.min(cost[cost.length-1][1],  cost[cost.length-1][2]));
 	   }
 There are a row of n houses, each house can be painted with one of the k colors. The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
 The cost of painting each house with a certain color is represented by a n x k cost matrix. For example, costs[0][0] is the cost of painting house 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2, and so on... Find the minimum cost to paint all houses.
